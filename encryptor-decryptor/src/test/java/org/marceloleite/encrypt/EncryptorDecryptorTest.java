1bb29b99e86a76ef21e376962e1a291aa2b6b631
MarceloLeite2604 <marceloleite2604@gmail.com>
2018-09-05 10:19:42 -0300

    Creates unit tests for EncryptorDecryptor

---
+++ encryptor-decryptor/src/test/java/org/marceloleite/encrypt/EncryptorDecryptorTest.java
@@ -0,0 +1,32 @@
+package org.marceloleite.encrypt;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EncryptorDecryptorTest {
+
+	private static final String CRYPTOGRAPHIC_ALGORYTHM = "DESede";
+
+	private EncryptorDecryptor encryptorDecryptor;
+	
+	private String key;
+
+	@Before
+	public void setUp() {
+		this.encryptorDecryptor = EncryptorDecryptor.builder()
+				.cryptographicAlgorythm(CRYPTOGRAPHIC_ALGORYTHM)
+				.feedbackMode("CBC")
+				.paddingScheme("PKCS5Padding")
+				.build();
+		
+		this.key = new KeyGenerator().generateKey(CRYPTOGRAPHIC_ALGORYTHM);
+	}
+	
+	@Test
+	public void testEncryptDecrypt() {
+		String text = "This text must be encrypted.";
+		String decryptedText = encryptorDecryptor.decrypt(encryptorDecryptor.encrypt(text, key), key);
+		Assert.assertEquals(text, decryptedText);
+	}
+}
