b6b281f19ac5188d75e05e555cb3c02f7efbc080
MarceloLeite2604 <marceloleite2604@gmail.com>
2019-08-30 09:25:39 -0300

    Adjustments on DurationUtil class

---
+++ util/src/main/java/com/github/marceloleite2604/util/time/TimeUtilMessageTemplates.java
@@ -0,0 +1,10 @@
+package com.github.marceloleite2604.util.time;
+
+final class TimeUtilMessageTemplates {
+
+	static final String DURATION_CANNOT_BE_NULL = "The duration cannot be null.";
+
+	private TimeUtilMessageTemplates() {
+		// Private constructor to avoid object instantiation.
+	}
+}
---
+++ util/src/main/java/com/github/marceloleite2604/util/time/TimeUtilRuntimeException.java
@@ -0,0 +1,15 @@
+package com.github.marceloleite2604.util.time;
+
+final class TimeUtilRuntimeException extends RuntimeException {
+
+	public TimeUtilRuntimeException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public TimeUtilRuntimeException(String message) {
+		super(message);
+	}
+
+	private static final long serialVersionUID = 1L;
+
+}
