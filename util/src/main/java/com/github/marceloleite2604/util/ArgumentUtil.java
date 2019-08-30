fa60cd96fc4e26ecce259e47f2d9438107b24a04
MarceloLeite2604 <marceloleite2604@gmail.com>
2019-08-30 07:48:44 -0300

    Add JavaDoc for ArgumentUtil class

--- util/src/main/java/com/github/marceloleite2604/util/ArgumentUtil.java
+++ util/src/main/java/com/github/marceloleite2604/util/ArgumentUtil.java
@@ -7,8 +7,18 @@ import java.util.regex.Pattern;
 
 public class ArgumentUtil {
 
-	public Optional<String> retrieveArgument(String[] args, String parameter) {
-		String argument = retrieveArgument(args, parameter, null);
+	/**
+	 * Retrieves the value of an optional argument.
+	 * 
+	 * @param arguments
+	 *            The arguments array.
+	 * @param name
+	 *            Name of the argument to be retrieved.
+	 * @return An {@link Optional} containing the argument value. If no argument was
+	 *         found, returns an empty {@link Optional}.
+	 */
+	public Optional<String> retrieveArgument(String[] arguments, String name) {
+		String argument = retrieveArgument(arguments, name, null);
 		if (argument == null) {
 			return Optional.empty();
 		} else {
@@ -16,18 +26,31 @@ public class ArgumentUtil {
 		}
 	}
 
-	public String retrieveArgument(String[] args, String parameter, String defaultValue) {
+	/**
+	 * Retrieves the value of an optional argument. If no value was found, returns
+	 * the value informed on {@code defaultValue} parameter.
+	 * 
+	 * @param arguments
+	 *            The arguments array.
+	 * @param name
+	 *            Name of the argument to be retrieved.
+	 * @param defaultValue
+	 *            Default value to be returned if the parameter is not found.
+	 * @return If the argument was found, the method returns its value. Otherwise,
+	 *         it will return the {@code defaultValue} parameter value.
+	 */
+	public String retrieveArgument(String[] arguments, String name, String defaultValue) {
 
 		String content = null;
 
-		List<String> arguments = Arrays.asList(args);
+		List<String> argumentsList = Arrays.asList(arguments);
 
-		int index = arguments.indexOf(parameter);
+		int index = argumentsList.indexOf(name);
 		if (index >= 0) {
-			if (index == arguments.size()) {
+			if (index == argumentsList.size()) {
 				return defaultValue;
 			}
-			content = arguments.get(index + 1);
+			content = argumentsList.get(index + 1);
 
 			if (Pattern.matches("-.", content)) {
 				return defaultValue;
@@ -41,8 +64,17 @@ public class ArgumentUtil {
 		}
 	}
 
-	public boolean checkArgument(String[] args, String parameter) {
-		return (Arrays.asList(args)
-				.indexOf(parameter) >= 0);
+	/**
+	 * Checks an argument was informed on an argument array.
+	 * 
+	 * @param arguments
+	 *            The arguments array.
+	 * @param name
+	 *            The name of the argument to be found.
+	 * @return {@code true} if the argument exists, {@code false} otherwise.
+	 */
+	public boolean checkArgument(String[] arguments, String name) {
+		return (Arrays.asList(arguments)
+				.indexOf(name) >= 0);
 	}
 }
