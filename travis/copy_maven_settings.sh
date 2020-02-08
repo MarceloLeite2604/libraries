667aac4704be57de09db21235c5d19a1ee9e3684
MarceloLeite2604 <marceloleite2604@gmail.com>
2020-02-07 21:30:57 -0300

    Adjust script to copy Maven settings on Travis

--- travis/copy_maven_settings.sh
+++ travis/copy_maven_settings.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cp travis/settings.xml ${HOME}/.m2/settings.xml;
-cp travis/settings-secure.xml ${HOME}/.m2/settings-secure.xml;
+cp travis/settings-security.xml ${HOME}/.m2/settings-security.xml;
 
 exit 0;
