30823cf9e7a3806b51f06b4c6e3c841f06b3b510
MarceloLeite2604 <marceloleite2604@gmail.com>
2020-02-13 16:22:20 -0300

    Adjust deployment scripts

--- travis/deploy/deploy_snapshot.sh
+++ travis/deploy/deploy_snapshot.sh
@@ -4,13 +4,13 @@ source "$(dirname ${BASH_SOURCE})/functions.sh";
 
 print_information;
 
-if [[ ! is_snapshot_branch ]];
+if [[ $(is_snapshot_branch) -eq 1 ]];
 then
   >&2 echo "Snapshot deployment will NOT be done because current branch $(retrieve_git_branch) is not ${snapshot_branch}.";
   exit 1;
 fi;
 
-if [[ ! is_snapshot_version ]];
+if [[ $(is_snapshot_version) -eq 1 ]];
 then
   >&2 echo "Snapshot deployment will NOT be done because project version $(retrieve_project_version) is not a snapshot.";
   exit 1;
